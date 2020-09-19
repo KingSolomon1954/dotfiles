1c2b88e6b466ae003f8e38323c0497fac658541d
KingSolomon <KingSolomon1954@gmail.com>
2020-09-19 09:54:46 -0700

    .bashdir/docker_setup.bash

--- .bashdir/docker_setup.bash
+++ .bashdir/docker_setup.bash
@@ -4,6 +4,4 @@
 #
 # -----------------------------------------------------------
 
-alias d-asciidoctor='run --rm  --volume="$PWD:/documents" asciidoctor/docker-asciidoctor:latest asciidoctor'
-
-alias d-jekyll='docker run --rm --volume="$PWD:/srv/jekyll" boekhoff/jekyll-asciidoc:latest jekyll'
+# TBS
