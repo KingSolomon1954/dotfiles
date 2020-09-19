4b0c90cca521270dbfb514febbfed34378495e5c
KingSolomon <KingSolomon1954@gmail.com>
2020-09-19 11:33:01 -0700

    aliases for docker/podman apps.

--- .bashdir/docker_setup.bash
+++ .bashdir/docker_setup.bash
@@ -4,4 +4,13 @@
 #
 # -----------------------------------------------------------
 
-# TBS
+if which podman 1>&2 > /dev/null; then
+    CNTR_EXEC=podman
+elif which docker 1>&2 > /dev/null; then
+    CNTR_EXEC=docker
+fi
+
+if [ -n CNTR_EXEC ]; then
+    alias jekyll='${CNTR_EXEC}   run --rm --volume="$PWD:/srv/jekyll" boekhoff/jekyll-asciidoc:latest jekyll'
+    alias asciidoc='${CNTR_EXEC} run --rm --volume="$PWD:/documents" asciidoctor/docker-asciidoctor:latest asciidoctor'
+fi
