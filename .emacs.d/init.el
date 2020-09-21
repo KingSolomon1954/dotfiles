2097083697d05f531d010c52ac193e8b17fda3fe
KingSolomon1954 <KingSolomon1954@gmail.com>
2020-09-20 19:49:48 -0700

    work-in-progress

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -24,3 +24,17 @@
 (require 'init-json)
 
 (provide 'init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (flycheck lua-mode adoc-mode yaml-mode markdown-mode json-mode ggtags expand-region use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
