21cb3fe46467d8a4de6e07373217afd084215b57
KingSolomon <KingSolomon1954@gmail.com>
2021-07-24 15:24:11 -0700

    Aliases for immediate window focus

---
+++ .bashdir/win_ops.bash
@@ -0,0 +1,10 @@
+# --------------------------------------------------
+#
+# Setup for window operations
+#
+# --------------------------------------------------
+
+alias 1='wmctrl -a "Upper Left"'
+alias 2='wmctrl -a "Lower Left"'
+alias 3='wmctrl -a "Upper Right"'
+alias 4='wmctrl -a "Lower Right"'
