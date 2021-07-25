87ec89a4f3c726781d28a1dacdba4e4367d15d08
KingSolomon <KingSolomon1954@gmail.com>
2021-07-24 17:29:42 -0700

    update

--- bin/to_win.bash
+++ bin/to_win.bash
@@ -1,10 +1,10 @@
 # Set focus to matching window title.
-# This is specific for my custom window titles.
+# This is specific to my custom window titles.
 
-# All the following to stay on the current desktop
-# while changing window focus using window titles 
-# that are the same across desktops
-# A plain wmctrl -a "name" jumps between desktops.
+# All the following logic to stay on the current 
+# desktop while changing window focus using window 
+# titles that are the same across desktops.
+# A plain wmctrl -a "name" will jump between desktops.
 
 # Window title to look for (can be partial)
 winTitle=$1
@@ -12,9 +12,9 @@ winTitle=$1
 # Which desktop is current. Look for asterisk
 _desktop=$(wmctrl -d | grep "*" | awk '{ print $1 }')
 
-# List windows matching a partial window title,
+# Find windows matching a partial window title,
 # then test for matching desktop and return the window
-_window=$(wmctrl -l | awk "/$winTitle/ { if (\$2 == ${_desktop}) \
+_window=$(wmctrl -l | awk "/$winTitle/ { if (\$2 == $_desktop) \
                           { print \$1; exit} }")
 
 # Switch to the window using the window ID
