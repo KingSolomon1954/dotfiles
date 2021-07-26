8275cb40d8d3af5ac2a4085af7ac36195f2a6520
hsolomon <hsolomon@viasat.com>
2021-07-26 16:55:03 -0700

    Direct jump to terminal windows via hotkey.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -5,9 +5,9 @@ lua scripting:    https://learnxinyminutes.com/docs/lua/
 samples:          https://github.com/Hammerspoon/hammerspoon/wiki/Sample-Configurations
 --]]
 
-------------------------
+-------------------------
 -- Four Windows placement
-------------------------
+-------------------------
 
 function fourTerminalsHome(doSizing)
   if homeOrOffice == "home" then
@@ -25,7 +25,7 @@ function fourTerminalsHome(doSizing)
   -- topLeft hs.geometry.point(4359.0,0.0)
   upperLeft:setTopLeft(4359.0,0.0)
   if doSizing then
-      upperLeft:setSize(590.0,545.0)
+      upperLeft:setSize(711.0,537.0)
   end
 
   local lowerLeft = hs.window.get("2 Lower Left")
@@ -36,7 +36,7 @@ function fourTerminalsHome(doSizing)
   -- topLeft hs.geometry.point(4359.0,546.0)
   lowerLeft:setTopLeft(4359.0,546.0)
   if doSizing then
-      lowerLeft:setSize(590.0,534.0)
+      lowerLeft:setSize(711.0,537.0)
   end
   
   local upperRight = hs.window.get("3 Upper Right")
@@ -47,18 +47,18 @@ function fourTerminalsHome(doSizing)
   -- topLeft hs.geometry.point(4950.0,0.0)
   upperRight:setTopLeft(4950.0,0.0)
   if doSizing then
-      upperRight:setSize(570.0,545.0)
+      upperRight:setSize(711.0,537.0)
   end
 
   local lowerRight = hs.window.get("4 Lower Right")
   -- print(lowerRight.id)
   -- print(lowerRight:size())
   -- print(lowerRight:topLeft())
-  -- size    hs.geometry.size(570.0,534.0)
+   -- size    hs.geometry.size(570.0,534.0)
   -- topLeft hs.geometry.point(4950.0,546.0)
   lowerRight:setTopLeft(4950.0,546.0)
   if doSizing then
-      lowerRight:setSize(570.0,534.0)
+      lowerRight:setSize(711.0,537.0)
   end
 
 end
@@ -66,7 +66,6 @@ end
 ------------------------
 
 function fourTerminalsHandler(eventName, params)
-    fourTerminalsHome(false)
     if params.doSizing == "true" then
         print("sizing is true")
         fourTerminalsHome(true)
@@ -102,5 +101,29 @@ if caffeine then
     setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
 end
 
-------------------------
+--------------------------------
+-- Jump to named terminal window
+--------------------------------
 
+function focusUpperLeftTerm()
+    hs.window.get("1 Upper Left"):focus()
+end
+
+function focusLowerLeftTerm()
+    hs.window.get("2 Lower Left"):focus()
+end
+
+function focusUpperRightTerm()
+    hs.window.get("3 Upper Right"):focus()
+end
+
+function focusLowerRightTerm()
+    hs.window.get("4 Lower Right"):focus()
+end
+
+hs.hotkey.bind({"ctrl","shift"}, "1", focusUpperLeftTerm)
+hs.hotkey.bind({"ctrl","shift"}, "2", focusLowerLeftTerm)
+hs.hotkey.bind({"ctrl","shift"}, "3", focusUpperRightTerm)
+hs.hotkey.bind({"ctrl","shift"}, "4", focusLowerRightTerm)
+
+------------------------
