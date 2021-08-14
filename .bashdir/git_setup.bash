a812555c491cad84c860602c93dca7f7bf33d428
hsolomon <hsolomon@viasat.com>
2021-08-14 13:38:59 -0700

    Adjust alias

--- .bashdir/git_setup.bash
+++ .bashdir/git_setup.bash
@@ -11,16 +11,20 @@ alias gc="git commit -m"
 alias gcw="git commit -m \"work-in-progress\""
 alias gac="git add -A && git commit -m "
 alias gp="git pull --recurse-submodules"
-alias gsu="git submodule update --remote --merge"
+alias gsu="git submodule foreach 'git pull || :'"
+
+# git submodule foreach ‘git checkout master || :'
+# git config -f .gitmodules submodule.docs-src/shared.branch master
+# git config -f .gitmodules submodule.docs-src/shared.update merge
 
 # Show the file's git history
 alias glog="git log --all --oneline --decorate --graph"
 
 # List the files involved in latest commit or specify a commit
-alias gfiles="git show --name-only"
+alias glsc="git show --name-only"
 
 # List the updated files in as yet unpushed commits
-alias gu='git log --name-only origin/master..HEAD'
+alias glsu='git log --name-only origin/master..HEAD'
 
 # Opens editor to set the current branch description
 alias gbe='git branch --edit-description'
