36875ea3d54fd2f6c475d34acf6e4d6fe65a2ea2
KingSolomon1954 <KingSolomon1954@gmail.com>
2021-08-30 22:33:46 -0700

    mv aliases to scripts

--- .bashdir/git_setup.bash
+++ .bashdir/git_setup.bash
@@ -11,23 +11,26 @@ alias gc="git commit -m"
 alias gcw="git commit -m \"work-in-progress\""
 alias gac="git add -A && git commit -m "
 alias gp="git pull --recurse-submodules"
-alias gsu="git submodule foreach 'git pull || :'"
+alias glog="git log --all --oneline --decorate --graph"
+
+# List the files involved in latest commit or specify a commit
+# alias glsc="git show --name-only"
+
+# List the updated files in as yet unpushed commits
+# alias glsu='git log --name-only origin/master..HEAD'
+
+# Opens editor to set the current branch description
+# alias gbe='git branch --edit-description'
+
+# alias gsu="git submodule foreach 'git pull || :'"
+
+# git config --global status.submoduleSummary true
+# git config --global diff.submodule log
 
 # git submodule foreach ‘git checkout master || :'
 # git config -f .gitmodules submodule.docs-src/shared.branch master
 # git config -f .gitmodules submodule.docs-src/shared.update merge
 
-# Show the file's git history
-alias glog="git log --all --oneline --decorate --graph"
-
-# List the files involved in latest commit or specify a commit
-alias glsc="git show --name-only"
-
-# List the updated files in as yet unpushed commits
-alias glsu='git log --name-only origin/master..HEAD'
-
-# Opens editor to set the current branch description
-alias gbe='git branch --edit-description'
 
 # -----------------------------------------------------------
 
