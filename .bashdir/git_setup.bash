0121eb58cfce1937805d4a0f7fba758454787a55
hsolomon <hsolomon@viasat.com>
2024-03-21 09:15:42 -0700

    add alias gsu

--- .bashdir/git_setup.bash
+++ .bashdir/git_setup.bash
@@ -13,6 +13,10 @@ alias gac="git add -A && git commit -m "
 alias gp="git pull --recurse-submodules"
 alias glog="git log --all --oneline --decorate --graph"
 alias glast="git log -1 HEAD"
+alias gsp="git switch -"
+alias gsm="git switch master"
+alias gbl="git branch --list -a"
+alias gsu="git submodule update --init --recursive"
 
 # List the files involved in latest commit or specify a commit
 # alias glsc="git show --name-only"
@@ -40,3 +44,30 @@ alias glast="git log -1 HEAD"
     
 # -----------------------------------------------------------
 
+# I don't use extra wrapper .sh files. My environment is Windows XP, git 1.7.1 on cygwin, and Beyond Compare 3. Following is my .git/config file.
+# 
+# [diff]
+#     tool = bc3
+# [difftool]
+#     prompt = false
+# [difftool "bc3"]
+#     #use cygpath to transform cygwin path $LOCAL (something like /tmp/U5VvP1_abc) to windows path, because bc3 is a windows software
+#     cmd = \"c:/program files/beyond compare 3/bcomp.exe\" "$(cygpath -w $LOCAL)" "$REMOTE"
+# [merge]
+#     tool = bc3
+# [mergetool]
+#     prompt = false
+# [mergetool "bc3"]
+#     #trustExitCode = true
+#     cmd = \"c:/program files/beyond compare 3/bcomp.exe\" "$LOCAL" "$REMOTE" "$BASE" "$MERGED"
+# 
+# Then, I use $ git difftool to compare and $ git mergetool to merge.
+# 
+# About trustExitCode: For a custom merge command, specify whether the
+# exit code of the merge command can be used to determine whether the
+# merge was successful. If this is not set to true then the merge target
+# file timestamp is checked and the merge assumed to have been
+# successful if the file has been updated, otherwise the user is
+# prompted to indicate the success of the merge.
+# 
+# --yehnan
